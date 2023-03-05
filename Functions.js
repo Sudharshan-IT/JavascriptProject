4e0a995f63b434afbd4336f706943b3e19022478
Sudharshan <sudharshanjavaguide@gmail.com>
2023-03-05 13:08:40 +0530

    Functions Added

---
+++ Functions.js
@@ -0,0 +1,24 @@
+//Functions
+//Bussiness logic called as Function
+//Functions are used to reuse the logic in the multiple places
+//we will define the function with "function" key workd
+
+//We have following  types of functions
+//Named Function
+//Anonymous Function or Arrow Function or CallBack Function or Nameless Function
+//Rest Parameters In Functions
+//Optional Parameters In Function
+//generators
+//Constructor Funcation
+//
+
+/***Fucntion with Name is called Named Function */
+/*
+Funcation Defination
+function welcome(arg 1,arg 2, arg 3......arg n){
+    //Bussiness Logic
+}
+Function Calling
+welcome(param1,param2,parm3,..........paramn);
+*/ 
+
